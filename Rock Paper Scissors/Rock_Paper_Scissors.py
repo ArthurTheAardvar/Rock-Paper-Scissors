ceeb1971cd3ff4090a25f72e41e6f18f2ea0a1be
768588 <768588@605-110-S16>
2023-10-27 13:07:32 -0600

    Add project files.

---
+++ Rock Paper Scissors/Rock_Paper_Scissors.py
@@ -0,0 +1,17 @@
+
+
+cases = int(input()) 
+
+for i in range(cases): #go through the other lines
+    line = input()
+    
+    line = line.split(" ") #split up the line by spaces
+    line = [str(num) for num in line]
+
+    if line[0] == "R":
+        "R" > "S"
+        "R" < "P"
+        "S" > "P"
+
+    if "R"
+    
